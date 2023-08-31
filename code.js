f29c24c3237c3b64094e0e632471dd20f949e601
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2023-08-31 20:54:56 +0400

    Change to log winner/loser and their choice

--- code.js
+++ code.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*  instead of comparing strings it's probably better to compare numbers
     0 - rock
     1 - paper
@@ -11,29 +10,17 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-=======
-function getComputerChoice() {
-    let rand = Math.floor(Math.random() * 3);
-
-    if (!rand) return "rock";
-    else if (rand == 1) return "paper";
-    return "scissors";
-}
-
-function playerChoice() {
->>>>>>> ec1b194101940551374a24b509038ae2e70b05e4
     let choice;
     while (true) {
         choice = prompt("Choose one: rock, paper or scissors");
 
         if (!choice)
             if (confirm("You didn't input anything.. Do you want to quit the game?"))
-<<<<<<< HEAD
                 return null;
             else continue;
 
         choice = choice.toLowerCase();
-        if (choice === "rock") 
+        if (choice === "rock")
             return 0;
         else if (choice === "paper")
             return 1;
@@ -43,30 +30,59 @@ function playerChoice() {
     }
 }
 
-function playRound()
-{
+function playRound() {
     let computerChoice = getComputerChoice();
     let playerChoice = getPlayerChoice();
 
-    if (playerChoice === null)
-        {
-            // add something later
-            return null;
-        }
-    
-    if (playerChoice === computerChoice)
+    if (playerChoice === null) {
+        // add something later
+        return null;
+    }
+
+    if (playerChoice === computerChoice) {
+        console.log("It's a Tie! You both choose the same thing!");
         return 0;
+    }
+
+    if (playerChoice === 0) {
+        if (computerChoice === 1) {
+            console.log("You Lose! Paper beats Rock");
+            return -1;
+        }
+        if (computerChoice === 2)
+        {
+            console.log("You Win! Rock beats Scissors");
+            return 1;
+        }
+    }
+
+    if (playerChoice === 1) {
+        if (computerChoice === 0) {
+            console.log("You Win! Paper beats Rock");
+            return -1;
+        }
+        if (computerChoice === 2)
+        {
+            console.log("You Lose! Scissors beat Paper");
+            return 1;
+        }
+    }
+
+    if (playerChoice === 2) {
+        if (computerChoice === 1) {
+            console.log("You Win! Scissors beat Paper");
+            return -1;
+        }
+        if (computerChoice === 2)
+        {
+            console.log("You Lose! Rock beats Scissors");
+            return 1;
+        }
+    }
+
+    /*
     if ((playerChoice === 2 && computerChoice === 0) || (playerChoice - computerChoice === -1))
         return -1;
     else return 1;
-=======
-                return choice;
-            else continue;
-
-        choice = choice.toLowerCase();
-        if (choice === "rock" || choice === "paper" || choice === "scissors")
-            return choice;
-        else alert("Incorrect input, must be 'rock', 'paper' or 'scissors' (case insensitive)");
-    }
->>>>>>> ec1b194101940551374a24b509038ae2e70b05e4
+    */
 }
