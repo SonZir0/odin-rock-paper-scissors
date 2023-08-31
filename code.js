ec1b194101940551374a24b509038ae2e70b05e4
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2023-08-31 18:20:17 +0400

    Add a function to take input from the player
    Done with prompts and alerts. Also added a check for cancel/empty input,
    that's asking a player if he wants to quit. If so, returns null

--- code.js
+++ code.js
@@ -1,7 +1,24 @@
-function getComputerChoice(){
+function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3);
 
     if (!rand) return "rock";
     else if (rand == 1) return "paper";
     return "scissors";
+}
+
+function playerChoice() {
+    let choice;
+    while (true) {
+        choice = prompt("Choose one: rock, paper or scissors");
+
+        if (!choice)
+            if (confirm("You didn't input anything.. Do you want to quit the game?"))
+                return choice;
+            else continue;
+
+        choice = choice.toLowerCase();
+        if (choice === "rock" || choice === "paper" || choice === "scissors")
+            return choice;
+        else alert("Incorrect input, must be 'rock', 'paper' or 'scissors' (case insensitive)");
+    }
 }
