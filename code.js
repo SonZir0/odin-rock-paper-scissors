e9a54194b30f96899518691f4866eabbde58088e
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2023-08-31 21:25:53 +0400

    Add a function to play a set of rounds
    The results displayed by the end of match. If canceled display the
    score 'so far'

--- code.js
+++ code.js
@@ -35,7 +35,7 @@ function playRound() {
     let playerChoice = getPlayerChoice();
 
     if (playerChoice === null) {
-        // add something later
+        console.log("Wrapping up..");
         return null;
     }
 
@@ -49,8 +49,7 @@ function playRound() {
             console.log("You Lose! Paper beats Rock");
             return -1;
         }
-        if (computerChoice === 2)
-        {
+        if (computerChoice === 2) {
             console.log("You Win! Rock beats Scissors");
             return 1;
         }
@@ -59,30 +58,55 @@ function playRound() {
     if (playerChoice === 1) {
         if (computerChoice === 0) {
             console.log("You Win! Paper beats Rock");
-            return -1;
-        }
-        if (computerChoice === 2)
-        {
-            console.log("You Lose! Scissors beat Paper");
             return 1;
+        }
+        if (computerChoice === 2) {
+            console.log("You Lose! Scissors beat Paper");
+            return -1;
         }
     }
 
     if (playerChoice === 2) {
         if (computerChoice === 1) {
             console.log("You Win! Scissors beat Paper");
-            return -1;
-        }
-        if (computerChoice === 2)
-        {
-            console.log("You Lose! Rock beats Scissors");
             return 1;
+        }
+        if (computerChoice === 2) {
+            console.log("You Lose! Rock beats Scissors");
+            return -1;
         }
     }
 
-    /*
+    /* Doesn't allow us to tell who choose what, and additional checks would defeat its purpose
+
     if ((playerChoice === 2 && computerChoice === 0) || (playerChoice - computerChoice === -1))
         return -1;
     else return 1;
     */
+}
+
+function game(rounds = 1) {
+    let playerScore = 0;
+    let computerScore = 0;
+    let result = 0;
+
+    for (let i = 0; i < rounds; i++) {
+        result = playRound();
+
+        if (result === null)
+            break;
+
+        if (result < 0)
+            computerScore++;
+        else if (result > 0)
+            playerScore++;
+    }
+
+    if (playerScore > computerScore)
+        console.log("You Won the game!");
+    else if (playerScore < computerScore)
+    console.log("You Lost the game, this time at the very least..");
+    else console.log("The game ended in a Tie!");
+
+    console.log(`Results:\nPlayer: ${playerScore}\nComputer: ${computerScore}`)
 }
